12f45e8175f2642c2f631040f9c717f0cee3f5b8
Empaty <davidchapanyan@gmail.com>
2024-06-24 13:38:00 +0500

    Added folders and some algorithms

---
+++ algoexpert/easy/TwoNumberSum/solution_hashtable.py
@@ -0,0 +1,20 @@
+# O(n) time | O(n) space
+
+def twoNumberSum(array, targetSum):
+    hashtable = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in hashtable:
+            return [potentialMatch, num]
+        else:
+            hashtable[num] = True
+    return []
+
+def main():
+    array = [3, 5, -4, 8, 11, 1, -1, 6]
+    targetSum = 10
+    
+    print(twoNumberSum(array, targetSum)) # => [-1, 11]
+
+if __name__ == "__main__":
+    main()
