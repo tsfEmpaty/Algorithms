db1c804c9b52a09c513594c19562b9339d106783
Empaty <davidchapanyan@gmail.com>
2024-06-27 21:26:23 +0500

    Added new challenge from AlgoExpert

---
+++ algoexpert/easy/ValidateSubsequence/solution_for_loop.py
@@ -0,0 +1,16 @@
+def isValidSubsequence(array, sequence):
+    for element in array:
+        if element == sequence[0]:
+            sequence.pop(0)
+            if not sequence:
+                return True
+    return False
+
+def main():
+    array = [5, 1, 22, 25, 6, -1, 8, 10]
+    sequence = [1, 6, -1, 10]
+    
+    print(isValidSubsequence(array, sequence)) # => True
+
+if __name__ == "__main__":
+    main()
---
+++ algoexpert/easy/ValidateSubsequence/solution_while_loop.py
@@ -0,0 +1,19 @@
+# O(n) time | O(1) space
+
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+def main():
+    array = [5, 1, 22, 25, 6, -1, 8, 10]
+    sequence = [1, 6, -1, 10]
+    
+    print(isValidSubsequence(array, sequence)) # => True
+
+if __name__ == "__main__":
+    main()
